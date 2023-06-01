91c6c26e58767e7878b025e7cd6dfe2dde375247
FrederikRehhoff <frede.rehhoff@gmail.com>
2023-06-01 15:51:06 +0200

    ros2 launch file works with turtlesim

---
+++ ros2_ws/build/py_pubsub/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/frederikrehhoff/Dev/Embedded_Linux_Project/ros2_ws/install/py_pubsub'
--- ros2_ws/src/emli_package/setup.py
+++ ros2_ws/src/emli_package/setup.py
@@ -1,15 +1,18 @@
+import os
+from glob import glob
 from setuptools import setup
 
 package_name = 'emli_package'
+
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        # ... Other data files
+        # Include all launch files.
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
