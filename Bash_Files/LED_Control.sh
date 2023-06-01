1f14bae80f60773aabf06869273f521cafeb48a7
Lucas <lucasthurnherr@hotmail.dk>
2023-06-01 11:29:34 +0200

    New LED_Control

--- Bash_Files/LED_Control.sh
+++ Bash_Files/LED_Control.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-if [ $1 = 0 ]
+if [ $1 = "red" ]
     then
-    if [ $2 = 0 ]
+    if [ $2 = "off" ]
         then
         # Send HTTP GET request to turn on the LED
         echo "Red LED turned off"
         curl -s "http://10.42.0.222/led/red/off" >/dev/null
         exit 0
     fi
-    if [ $2 = 1 ]
+    if [ $2 = "on" ]
+        then
         then
         # Send HTTP GET request to turn on the LED
         echo "Red LED turned on"
@@ -17,16 +18,16 @@ if [ $1 = 0 ]
         exit 0
     fi
 fi
-if [ $1 = 1 ]
+if [ $1 = "yellow" ]
     then
-    if [ $2 = 0 ]
+    if [ $2 = "off" ]
         then
         # Send HTTP GET request to turn on the LED
         echo "Red LED turned off"
         curl -s "http://10.42.0.222/led/yellow/off" >/dev/null
         exit 0
     fi
-    if [ $2 = 1 ]
+    if [ $2 = "on" ]
         then
         # Send HTTP GET request to turn on the LED
         echo "Red LED turned on"
@@ -34,16 +35,16 @@ if [ $1 = 1 ]
         exit 0
     fi
 fi
-if [ $1 = 2 ]
+if [ $1 = "green" ]
     then
-    if [ $2 = 0 ]
+    if [ $2 = "off" ]
         then
         # Send HTTP GET request to turn on the LED
         echo "Red LED turned off"
         curl -s "http://10.42.0.222/led/green/off" >/dev/null
         exit 0
     fi
-    if [ $2 = 1 ]
+    if [ $2 = "on" ]
         then
         # Send HTTP GET request to turn on the LED
         echo "Red LED turned on"
