f408aae6acec855a08c31df07b1b0de77befa0e0
BueRuben <Andersjohannsen@outlook.com>
2023-06-02 10:38:07 +0200

    Read_Sensor

--- Bash_Files/Read_Sensor.sh
+++ Bash_Files/Read_Sensor.sh
@@ -6,16 +6,8 @@ baud_rate=115200  # Update with the correct baud rate
 #screen -L $serial_port $baud_rate
 stty -F $serial_port $baud_rate
 #cat $serial_port
-echo "Started porgram"
-data=""
-while [ -z "$data" ]; do
-	echo "Ran the while loop"  
-	data=$(timeout 5 cat $serial_port)
-done
-echo "Stoped the while loop"
 
-
-#data=$(timeout 1 cat $serial_port)
-#if [ -n "$data" ]; then
-#  echo "$data"
-#fi
+data=$(timeout 1 cat $serial_port)
+if [ -n "$data" ]; then
+  echo "$data"
+fi
