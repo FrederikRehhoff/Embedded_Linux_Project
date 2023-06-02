78ec3c6f1e1d75690667ac4b8470436a445cb4b0
BueRuben <Andersjohannsen@outlook.com>
2023-06-02 10:20:50 +0200

    yes

--- Bash_Files/Read_Sensor.sh
+++ Bash_Files/Read_Sensor.sh
@@ -6,7 +6,17 @@ baud_rate=115200  # Update with the correct baud rate
 #screen -L $serial_port $baud_rate
 stty -F $serial_port $baud_rate
 #cat $serial_port
-data=$(timeout 1 cat $serial_port)
-if [ -n "$data" ]; then
-  echo "$data"
-fi
+data=""
+while [ -z "$data" ]; do
+  read -p "Press Enter to start the pump: " _  # Wait for any key press or Enter
+  echo "start" > $serial_port
+
+  data=$(timeout 5 cat $serial_port)
+done
+
+
+
+#data=$(timeout 1 cat $serial_port)
+#if [ -n "$data" ]; then
+#  echo "$data"
+#fi
