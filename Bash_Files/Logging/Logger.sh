0b94c83fdc5467b170c7833676c778bdfb74ba7c
BueRuben <Andersjohannsen@outlook.com>
2023-06-03 15:24:09 +0200

    added changes in the Logging folder

--- Bash_Files/Logging/Logger.sh
+++ Bash_Files/Logging/Logger.sh
@@ -8,14 +8,20 @@ Pump_Water_Alarm=$4
 Plant_Water_Alarm=$5
 
 
-printf "\n$Date|$Moisture_Level|$Ambient_Light|$Pump_Active|$Pump_Water_Alarm|$Plant_Water_Alarm\n">>Log.dat
 printf "$(date '+%d/%m/%Y %H:%M:%S')|$Moisture_Level\n">>Plot_Moisture_Data.dat
 printf "$(date '+%d/%m/%Y %H:%M:%S')|$Ambient_Light\n">>Plot_Ambient_Light_Data.dat
-printf "$(date '+%d/%m/%Y %H:%M:%S')|$Pump_Active\n">>Plot_Pump_Active_Data.dat
 printf "$(date '+%d/%m/%Y %H:%M:%S')|$Pump_Water_Alarm\n">>Plot_Pump_Water_Alarm_Data.dat
 printf "$(date '+%d/%m/%Y %H:%M:%S')|$Plant_Water_Alarm\n">>Plot_Plant_Water_Alarm_Data.dat
+if [ $4 = ½ ] && [ $5 = 0 ]
+	then
+	printf "\n$Date|$Moisture_Level|$Ambient_Light|$Pump_Active|$Pump_Water_Alarm|$Plant_Water_Alarm\n">>Log.dat
+	printf "$(date '+%d/%m/%Y %H:%M:%S')|$Pump_Active\n">>Plot_Pump_Active_Data.dat
+else
+	printf "\n$Date|$Moisture_Level|$Ambient_Light|0|$Pump_Water_Alarm|$Plant_Water_Alarm\n">>Log.dat
+	printf "$(date '+%d/%m/%Y %H:%M:%S')|0\n">>Plot_Pump_Active_Data.dat
+fi
+
 column Log.dat -t -s "|">Log.csv
 
 txt2html --make_table --infile "Log.csv" --outfile "Plots/Log.html"
 ./Plotter.sh
-./Server_Health_Logger.sh
